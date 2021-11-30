659c260a407a08c3f7e180fa3a315111b60a541e
unknown <you@example.com>
2021-11-30 09:10:50 +0600

    updated

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -55,7 +55,7 @@ export default function AboutMe(props) {
               {renderHighlight()}
             </div>
             <div className="about-me-options">
-              <button style={{ backgroundColor: "#24263B", color: "#fff", border: '1px solid #FF5823' }} className="btn primary-btn"> Hire Me </button>
+           
               <a href="naimur's resume.pdf" download="naimur naimur's resume.pdf">
                 <button style={{ backgroundColor: '#FF5823', color: '#fff' }} className="btn highlighted-btn">Get Resume</button>
               </a>
